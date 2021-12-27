c9eeff2a4782062067b19bb34b8f3d80fd04daf0
Jahid <jahidulhaque767@gmail.com>
2021-12-28 02:52:03 +0600

    added design in give with paypal

--- src/components/Home/GiveWithPaypal/GiveWithPaypal.js
+++ src/components/Home/GiveWithPaypal/GiveWithPaypal.js
@@ -1,10 +1,41 @@
+import { Box, Container, Grid, Typography } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
 import "./GiveWithPaypal.css";
 
 const GiveWithPaypal = () => {
   return (
     <div>
-      <h2>Give with paypal</h2>
+      <Container>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6}>
+            <img
+              src="https://i.ibb.co/RyFQy3P/give-img.jpg"
+              alt="give with paypal"
+              style={{ width: "100%", height: "auto" }}
+            />
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            md={6}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Box>
+              <Typography variant="h3">Give with PayPal</Typography>
+              <Typography sx={{ my: 3, fontWeight: "medium" }}>
+                Make a difference by supporting causes and fundraisers close to
+                home and heart.
+              </Typography>
+              <Link to="#">Give through the app</Link>
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 };
