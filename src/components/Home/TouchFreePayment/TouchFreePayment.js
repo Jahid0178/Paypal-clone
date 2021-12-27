43c0d8afba67c4ef66fcaf24fdefa79918aad848
Jahid <jahidulhaque767@gmail.com>
2021-12-28 02:36:42 +0600

    added design in touch free payment

--- src/components/Home/TouchFreePayment/TouchFreePayment.js
+++ src/components/Home/TouchFreePayment/TouchFreePayment.js
@@ -1,11 +1,42 @@
+import { Box, Container, Grid, Typography } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
 import "./TouchFreePayment.css";
 
 const TouchFreePayment = () => {
   return (
-    <div>
-      <h2>Touch Free Payment</h2>
-    </div>
+    <>
+      <Container sx={{ py: 5 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6}>
+            <img
+              src="https://i.ibb.co/86bBpF2/touchfree-img.jpg"
+              alt="touch-free"
+              style={{ width: "100%", height: "auto" }}
+            />
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            md={6}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Box>
+              <Typography variant="h3">Touch-free payments</Typography>
+              <Typography sx={{ fontWeight: "medium", my: 3 }}>
+                Leave the cash and cards at home. When it comes to paying with
+                the app, the only thing you’ll need to reach for is your phone.
+              </Typography>
+              <Link to="#">Learn more about PayPal QR codes</Link>
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
   );
 };
 
