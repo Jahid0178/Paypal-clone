f29cb89c5febb1b3106c889a8af9a9ceee75f00b
Jahid <jahidulhaque767@gmail.com>
2021-12-28 03:14:43 +0600

    added design in brand without logo

--- src/components/Home/Brands/Brands.js
+++ src/components/Home/Brands/Brands.js
@@ -1,10 +1,22 @@
+import { Container, Grid, Typography } from "@mui/material";
 import React from "react";
 import "./Brands.css";
 
 const Brands = () => {
   return (
     <div>
-      <h2>Brands</h2>
+      <Container>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={5}>
+            <Typography variant="h2" sx={{ fontWeight: "medium" }}>
+              Check out with millions of brands you love
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={7}>
+            <Typography>All brands in here</Typography>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 };
