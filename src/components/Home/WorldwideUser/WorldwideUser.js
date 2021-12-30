34560b7e1c4f7c2a93b466f1c3972687c19ec3be
Jahid <jahidulhaque767@gmail.com>
2021-12-31 05:14:54 +0600

    added container on world wide user

--- src/components/Home/WorldwideUser/WorldwideUser.js
+++ src/components/Home/WorldwideUser/WorldwideUser.js
@@ -1,11 +1,14 @@
+import { Container } from "@mui/material";
 import React from "react";
 import "./WorldwideUser.css";
 
 const WorldwideUser = () => {
   return (
-    <div>
-      <h2>World wide user</h2>
-    </div>
+    <>
+      <Container>
+        <h2>World wide user</h2>
+      </Container>
+    </>
   );
 };
 
