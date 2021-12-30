ba79cf7e93fef8fdb751bcbb75440ac28563b09f
Jahid <jahidulhaque767@gmail.com>
2021-12-31 04:33:32 +0600

    complete connect paypal

--- src/components/Home/ConnectPaypal/ConnectPaypal.js
+++ src/components/Home/ConnectPaypal/ConnectPaypal.js
@@ -1,11 +1,17 @@
+import { Box, Container, Typography } from "@mui/material";
 import React from "react";
 import "./ConnectPaypal.css";
 
 const ConnectPaypal = () => {
   return (
-    <div>
-      <h2>Connect Paypal</h2>
-    </div>
+    <Box sx={{ border: "1px solid #ddd" }}>
+      <Container sx={{ py: 3 }}>
+        <Typography variant="h6" sx={{ textAlign: "center", py: 3 }}>
+          Need to connect PayPal with your online store?{" "}
+          <a href="#">Sign up for a Business account</a>
+        </Typography>
+      </Container>
+    </Box>
   );
 };
 
